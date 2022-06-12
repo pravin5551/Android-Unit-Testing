52da56aee45f7755dd1fa9114a61bd3fa590d426
Pravin Pawar <pravinvpawar5551@gmail.com>
2022-06-12 16:28:41 +0530

    Tax test file uploaded

--- UnitTesting/app/src/test/java/com/example/unittesting/TaxTest.kt
+++ UnitTesting/app/src/test/java/com/example/unittesting/TaxTest.kt
@@ -1,5 +1,30 @@
 package com.example.unittesting
 
-import junit.framework.TestCase
 
-class TaxTest : TestCase()
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class TaxTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun calculateTax() {
+    }
+
+    @Test
+    fun calculateIncome() {
+    }
+
+    @Test
+    fun `test for wrong error`(){
+
+    }
+}
